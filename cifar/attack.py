f736cf47799b5ababd7ae0c5f2dc1b4295516f68
Zhedong Zheng <Zhedong.Zheng@student.uts.edu.au>
2018-06-29 13:48:17 +0800

    Create attack.py

---
+++ cifar/attack.py
@@ -0,0 +1,246 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function, division
+
+import argparse
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+from torch.autograd import Variable
+import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
+import time
+import os
+import scipy.io
+import torch.utils.data as data
+import models.cifar as models
+from PIL import Image
+
+######################################################################
+# Options
+# --------
+parser = argparse.ArgumentParser(description='Training')
+parser.add_argument('--gpu_ids',default='1', type=str,help='gpu_ids: e.g. 0  0,1,2  0,2')
+parser.add_argument('--which_epoch',default='last', type=str, help='0,1,2,3...or last')
+parser.add_argument('--test_dir',default='/home/zzd/Market/pytorch',type=str, help='./test_data')
+parser.add_argument('--name', default='resnet20', type=str, help='save model path')
+parser.add_argument('--batchsize', default=200, type=int, help='batchsize')
+parser.add_argument('--use_dense', action='store_true', help='use densenet121' )
+parser.add_argument('--depth', default=20, type=int, help='model depth')
+parser.add_argument('--method_id', default=1, type=int, help='1.fast || 2.least likely || 3.label smooth')
+parser.add_argument('--rate', default=2, type=int, help='attack rate')
+###########################################
+# python test.py --name resnet20 --depth 20
+##########################################
+
+opt = parser.parse_args()
+
+str_ids = opt.gpu_ids.split(',')
+#which_epoch = opt.which_epoch
+name = opt.name
+test_dir = opt.test_dir
+######################################################################
+# Load Data
+# ---------
+#
+# We will use torchvision and torch.utils.data packages for loading the
+# data.
+#
+data_transforms = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+])
+
+dataloader = datasets.CIFAR10
+testset = dataloader(root='./data', train=False, download=False, transform=data_transforms)
+testloader = data.DataLoader(testset, batch_size=opt.batchsize, shuffle=False, num_workers=8)
+
+class_names =  ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+use_gpu = torch.cuda.is_available()
+
+######################################################################
+# Load model
+#---------------------------
+def load_network(network):
+    save_path = os.path.join('./checkpoint',name,'model_best.pth.tar')
+    checkpoint = torch.load(save_path)
+    network.load_state_dict(checkpoint['state_dict'])
+    return network
+
+
+
+def attack(model,dataloaders, method_id):
+    count = 0
+    score = 0
+    score5 = 0
+    for data in dataloaders:
+        img, label = data
+        n, c, h, w = img.size()
+        count += n
+        inputs = Variable(img.cuda(), requires_grad = True)
+        outputs = model(inputs) 
+        #-----------------attack-------------------
+        # The input has been whiten.
+        # So when we recover, we need to use a alpha
+        alpha = 1.0 / (0.2 * 255.0)
+        criterion = nn.CrossEntropyLoss()
+        inputs_copy = Variable(inputs.data, requires_grad = False)
+        diff = torch.FloatTensor(inputs.shape).zero_()
+        diff = Variable(diff.cuda(), requires_grad = False)
+        #1. FGSM, GradientSignAttack
+        if method_id == 1:
+            _, preds = torch.max(outputs.data, 1)
+            labels = Variable(preds.cuda())
+            loss = criterion(outputs, labels)
+            loss.backward()
+            inputs = inputs + torch.sign(inputs.grad) * opt.rate * alpha
+            inputs = clip(inputs,n)
+        #2. IterativeGradientSignAttack
+        elif method_id == 2:
+            _, preds = torch.max(outputs.data, 1)
+            labels = Variable(preds.cuda())
+            for iter in range( round(min(1.25 * opt.rate, opt.rate+4))):
+                loss = criterion(outputs, labels)
+                loss.backward()
+                diff += torch.sign(inputs.grad)
+                mask_diff = diff.abs() > opt.rate
+                diff[mask_diff] = opt.rate * torch.sign(diff[mask_diff])
+                inputs = inputs_copy + diff * 1.0  * alpha # we use 1 instead of opt.rate
+                inputs = clip(inputs,n)
+                inputs = Variable(inputs.data, requires_grad=True)
+                outputs = model(inputs)
+        #3. Iterative Least-likely method
+        elif method_id == 3:
+            # least likely label is fixed
+            _, ll_preds = torch.min(outputs.data, 1)
+            ll_label = Variable(ll_preds, requires_grad=False)
+            for iter in range( round(min(1.25 * opt.rate, opt.rate+4))):
+                loss = criterion(outputs, ll_label)
+                loss.backward()
+                diff += torch.sign(inputs.grad)
+                mask_diff = diff.abs() > opt.rate
+                diff[mask_diff] = opt.rate * torch.sign(diff[mask_diff])
+                inputs = inputs_copy - diff * 1.0 * alpha # we use 1 instead of opt.rate
+                inputs = clip(inputs,n)
+                inputs = Variable(inputs.data, requires_grad=True)
+                outputs = model(inputs)
+        #4. Label-smooth method
+        elif method_id == 4:
+            batch_size = inputs.shape[0]
+            smooth_label = torch.ones(batch_size, 10) /10.0
+            target = Variable(smooth_label.cuda(), requires_grad=False)
+            criterion2 = nn.MSELoss()
+            sm = nn.Softmax(dim = 1) #softmax work on the second dim (sum of the 751 elements = 1)
+            for iter in range( round(min(1.25 * opt.rate, opt.rate+4))):
+                sm_outputs = sm(outputs)
+                loss2 = criterion2(sm_outputs, target)
+                loss2.backward()
+                prob,_ = torch.max(sm_outputs,1)
+                #print('iter:%d smooth-loss:%4f max-pre:%4f'%(iter, loss2.data[0],torch.mean(prob)))
+                diff += torch.sign(inputs.grad)
+                mask_diff = diff.abs() > opt.rate
+                diff[mask_diff] = opt.rate * torch.sign(diff[mask_diff])
+                inputs = inputs_copy - diff * 1.0 * alpha 
+                inputs = clip(inputs,n)
+                inputs = Variable(inputs.data, requires_grad=True)
+                outputs = model(inputs)
+        #5. MSE on feature
+        elif method_id == 5:
+            #remove classifier
+            outputs = get_feature(model,inputs)
+            fnorm = torch.norm(outputs, p=2, dim=1, keepdim=True)
+            outputs = outputs.div(fnorm.expand_as(outputs))
+            feature_dim = outputs.shape[1]
+            batch_size = inputs.shape[0]
+            #zero_feature = torch.zeros(batch_size,feature_dim)
+            target = Variable(-outputs.data, requires_grad=False)
+            criterion2 = nn.MSELoss()
+            #s = target*target
+            #print(torch.sum(s))
+            for iter in range( round(min(1.25 * opt.rate, opt.rate+4))):
+                loss2 = criterion2(outputs, target)
+                loss2.backward()
+                diff += torch.sign(inputs.grad)
+                mask_diff = diff.abs() > opt.rate
+                diff[mask_diff] = opt.rate * torch.sign(diff[mask_diff])
+                inputs = inputs_copy - diff * 1.0 * alpha
+                inputs = clip(inputs,n)
+                inputs = Variable(inputs.data, requires_grad=True)
+                outputs = get_feature(model, inputs)
+                fnorm = torch.norm(outputs, p=2, dim=1, keepdim=True)
+                outputs = outputs.div(fnorm.expand_as(outputs))
+            #print( torch.sum(outputs*target))
+        else:
+            print('unknow method id')
+
+        outputs = model(inputs)
+        outputs = outputs.data.cpu()
+        _, preds = outputs.topk(5, dim=1)
+        correct = preds.eq(label.view(n,1).expand_as(preds))
+        score += torch.sum(correct[:,0])
+        score5 += torch.sum(correct)
+    print( '%.4f | %.4f'%(score/count, score5/count))
+    return
+
+######################################################################
+# Load Collected data Trained model
+#print('-------test-----------')
+
+model_structure = models.__dict__['resnet'](
+                    num_classes=10,
+                    depth=opt.depth,
+                )
+
+model = torch.nn.DataParallel(model_structure).cuda()
+model = load_network(model)
+# Change to test mode
+model = model.eval()
+
+def get_feature(model,x):
+        x = model.module.conv1(x)
+        x = model.module.bn1(x)
+        x = model.module.relu(x)    # 32x32
+
+        x = model.module.layer1(x)  # 32x32
+        x = model.module.layer2(x)  # 16x16
+        x = model.module.layer3(x)  # 8x8
+
+        x = model.module.avgpool(x)
+        x = x.view(x.size(0), -1)
+        #x = self.fc(x)
+
+        return x
+
+#######################################################################
+# Creat Up bound and low bound
+# Clip 
+zeros = np.zeros((32,32,3),dtype=np.uint8)
+zeros = Image.fromarray(zeros) 
+zeros = data_transforms(zeros)
+
+ones = 255*np.ones((32,32,3), dtype=np.uint8)
+ones = Image.fromarray(ones)
+ones = data_transforms(ones)
+
+zeros,ones = zeros.cuda(),ones.cuda()
+
+def clip(inputs, batch_size):
+    inputs = inputs.data
+    for i in range(batch_size):
+        inputs[i] = clip_single(inputs[i])
+    inputs = Variable(inputs.cuda())
+    return inputs
+
+def clip_single(input):       
+    low_mask = input<zeros
+    up_mask = input>ones
+    input[low_mask] = zeros[low_mask]
+    input[up_mask] = ones[up_mask]
+    return input
+
+#################################################################
+# Attack
+attack(model,testloader,opt.method_id)
