69a279cdd869e55e470658e0fcaab655562ae1e6
layumi <zdzheng12@gmail.com>
2019-06-24 16:19:03 +1000

    update cub

--- cub/train.py
+++ cub/train.py
@@ -8,49 +8,83 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.optim import lr_scheduler
 from torch.autograd import Variable
-import numpy as np
-import torchvision
-from torchvision import datasets, models, transforms
+from torchvision import datasets, transforms
+import torch.backends.cudnn as cudnn
 import matplotlib
 matplotlib.use('agg')
 import matplotlib.pyplot as plt
-from PIL import Image
+#from PIL import Image
+import copy
 import time
 import os
-from model import ft_net, ft_net_dense, PCB
+from losses import AngleLoss, ArcLoss
+from model import ft_net, ft_net_dense, ft_net_NAS, PCB
 from random_erasing import RandomErasing
-import json
+from random_erasing_plus import RandomErasingPlus
+import yaml
+import math
+from shutil import copyfile
+from autoaugment import ReIDPolicy
+from utils import update_average, get_model_list, load_network, save_network, make_weights_for_balanced_classes
 
+version =  torch.__version__
+#fp16
+try:
+    from apex.fp16_utils import *
+    from apex import amp, optimizers
+except ImportError: # will be 3.x series
+    print('This is not an error. If you want to use low precision, i.e., fp16, please install the apex with cuda support (https://github.com/NVIDIA/apex) and update pytorch to 1.0')
 ######################################################################
 # Options
 # --------
 parser = argparse.ArgumentParser(description='Training')
 parser.add_argument('--gpu_ids',default='0', type=str,help='gpu_ids: e.g. 0  0,1,2  0,2')
 parser.add_argument('--name',default='ft_ResNet50', type=str, help='output model name')
+parser.add_argument('--pool',default='avg', type=str, help='pool avg')
 parser.add_argument('--data_dir',default='./images',type=str, help='training dir path')
 parser.add_argument('--train_all', action='store_true', help='use all training data' )
 parser.add_argument('--color_jitter', action='store_true', help='use color jitter in training' )
 parser.add_argument('--batchsize', default=32, type=int, help='batchsize')
+parser.add_argument('--stride', default=2, type=int, help='stride')
+parser.add_argument('--pad', default=10, type=int, help='padding')
+parser.add_argument('--h', default=256, type=int, help='height')
+parser.add_argument('--w', default=256, type=int, help='width')
 parser.add_argument('--erasing_p', default=0, type=float, help='Random Erasing probability, in [0,1]')
+parser.add_argument('--erasing_p_plus', default=0, type=float, help='Random Erasing probability, in [0,1]')
 parser.add_argument('--use_dense', action='store_true', help='use densenet121' )
+parser.add_argument('--use_NAS', action='store_true', help='use NAS' )
+parser.add_argument('--warm_epoch', default=0, type=int, help='the first K epoch that needs warm up')
+parser.add_argument('--lr', default=0.05, type=float, help='learning rate')
+parser.add_argument('--moving_avg', default=1.0, type=float, help='moving average')
+parser.add_argument('--droprate', default=0.5, type=float, help='drop rate')
 parser.add_argument('--PCB', action='store_true', help='use PCB+ResNet50' )
+parser.add_argument('--DA', action='store_true', help='use Color Data Augmentation' )
+parser.add_argument('--resume', action='store_true', help='use resume trainning' )
+parser.add_argument('--fp16', action='store_true', help='use float16 instead of float32, which will save about 50% memory' )
 opt = parser.parse_args()
 
+if opt.resume:
+    model, opt, start_epoch = load_network(opt.name, opt)
+else:
+    start_epoch = 0
+
+
+fp16 = opt.fp16
 data_dir = opt.data_dir
 name = opt.name
-str_ids = opt.gpu_ids.split(',')
-gpu_ids = []
-for str_id in str_ids:
-    gid = int(str_id)
-    if gid >=0:
-        gpu_ids.append(gid)
+
+if not opt.resume:
+    str_ids = opt.gpu_ids.split(',')
+    gpu_ids = []
+    for str_id in str_ids:
+        gid = int(str_id)
+        if gid >=0:
+            gpu_ids.append(gid)
+    opt.gpu_ids = gpu_ids
 
 # set gpu ids
 if len(gpu_ids)>0:
-    torch.cuda.set_device(gpu_ids[0])
-#print(gpu_ids[0])
-
-
+    cudnn.benchmark = True
 ######################################################################
 # Load Data
 # ---------
@@ -58,37 +92,32 @@ if len(gpu_ids)>0:
 
 transform_train_list = [
         #transforms.RandomResizedCrop(size=128, scale=(0.75,1.0), ratio=(0.75,1.3333), interpolation=3), #Image.BICUBIC)
-        transforms.Resize(256, interpolation=3),
-        transforms.RandomCrop((256,256)),
+        transforms.Resize((opt.h, opt.w), interpolation=3),
+        transforms.Pad( opt.pad, padding_mode='edge'),
+        transforms.RandomCrop((opt.h, opt.w)),
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
         ]
 
 transform_val_list = [
-        transforms.Resize(size=(256,256),interpolation=3), #Image.BICUBIC
+        transforms.Resize(size=(opt.h, opt.w),interpolation=3), #Image.BICUBIC
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
         ]
 
-if opt.PCB:
-    transform_train_list = [
-        transforms.Resize((384,192), interpolation=3),
-        transforms.RandomHorizontalFlip(),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-        ]
-    transform_val_list = [
-        transforms.Resize(size=(384,192),interpolation=3), #Image.BICUBIC
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-        ]
 
 if opt.erasing_p>0:
-    transform_train_list = transform_train_list + [RandomErasing(opt.erasing_p)]
+    transform_train_list = transform_train_list +  [RandomErasing(probability = opt.erasing_p, mean=[0.0, 0.0, 0.0])]
+
+if opt.erasing_p_plus>0:
+    transform_train_list = transform_train_list +  [RandomErasingPlus(probability = opt.erasing_p_plus, mean=[0.0, 0.0, 0.0])]
 
 if opt.color_jitter:
     transform_train_list = [transforms.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.1, hue=0)] + transform_train_list
+
+if opt.DA:
+    transform_train_list = [ReIDPolicy()] + transform_train_list
 
 print(transform_train_list)
 data_transforms = {
@@ -97,26 +126,23 @@ data_transforms = {
 }
 
 
-train_all = ''
-if opt.train_all:
-     train_all = '_all'
+train_all = '_all'
 
 image_datasets = {}
 image_datasets['train'] = datasets.ImageFolder(os.path.join(data_dir, 'train' + train_all),
                                           data_transforms['train'])
-image_datasets['val'] = datasets.ImageFolder(os.path.join(data_dir, 'val'),
-                                          data_transforms['val'])
 
 dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=opt.batchsize,
-                                             shuffle=True, num_workers=16)
-              for x in ['train', 'val']}
-dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
+                                             shuffle=True, num_workers=2, pin_memory=True) # 8 workers may work faster
+              for x in ['train']}
+dataset_sizes = {x: len(image_datasets[x]) for x in ['train']}
 class_names = image_datasets['train'].classes
 
 use_gpu = torch.cuda.is_available()
 
+since = time.time()
 inputs, classes = next(iter(dataloaders['train']))
-
+print(time.time()-since)
 ######################################################################
 # Training the model
 # ------------------
@@ -137,18 +163,21 @@ y_err = {}
 y_err['train'] = []
 y_err['val'] = []
 
-def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
+def train_model(model, model_test, criterion, optimizer, scheduler, num_epochs=25):
     since = time.time()
 
-    best_model_wts = model.state_dict()
-    best_acc = 0.0
+    #best_model_wts = model.state_dict()
+    #best_acc = 0.0
+    warm_up = 0.1 # We start from the 0.1*lrRate
+    warm_iteration = round(dataset_sizes['train']/opt.batchsize)*opt.warm_epoch # first 5 epoch
 
-    for epoch in range(num_epochs):
+    for epoch in range(num_epochs-start_epoch):
+        epoch = epoch + start_epoch
         print('Epoch {}/{}'.format(epoch, num_epochs - 1))
         print('-' * 10)
-
+        
         # Each epoch has a training and validation phase
-        for phase in ['train', 'val']:
+        for phase in ['train']:
             if phase == 'train':
                 scheduler.step()
                 model.train(True)  # Set model to training mode
@@ -156,24 +185,30 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
                 model.train(False)  # Set model to evaluate mode
 
             running_loss = 0.0
-            running_corrects = 0
+            running_corrects = 0.0
             # Iterate over data.
             for data in dataloaders[phase]:
                 # get the inputs
                 inputs, labels = data
-                #print(inputs.shape)
-                # wrap them in Variable
+                now_batch_size,c,h,w = inputs.shape
+                if now_batch_size<opt.batchsize: # skip the last batch
+                    continue
                 if use_gpu:
-                    inputs = Variable(inputs.cuda())
-                    labels = Variable(labels.cuda())
+                    inputs = Variable(inputs.cuda().detach())
+                    labels = Variable(labels.cuda().detach())
                 else:
                     inputs, labels = Variable(inputs), Variable(labels)
-
+ 
                 # zero the parameter gradients
                 optimizer.zero_grad()
 
                 # forward
-                outputs = model(inputs)
+                if phase == 'val':
+                    with torch.no_grad():
+                        outputs = model(inputs)
+                else:
+                    outputs = model(inputs)
+
                 if not opt.PCB:
                     _, preds = torch.max(outputs.data, 1)
                     loss = criterion(outputs, labels)
@@ -192,39 +227,56 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
                         loss += criterion(part[i+1], labels)
 
                 # backward + optimize only if in training phase
+                if epoch<opt.warm_epoch and phase == 'train': 
+                    warm_up = min(1.0, warm_up + 0.9 / warm_iteration)
+                    loss *= warm_up
+
                 if phase == 'train':
-                    loss.backward()
+                    if fp16: # we use optimier to backward loss
+                        with amp.scale_loss(loss, optimizer) as scaled_loss:
+                            scaled_loss.backward()
+                    else:
+                        loss.backward()
                     optimizer.step()
+                    ##########
+                    if opt.moving_avg<1.0:
+                        update_average(model_test, model, opt.moving_avg)
 
                 # statistics
-                running_loss += loss.data[0]
-                running_corrects += torch.sum(preds == labels.data)
+                if int(version[0])>0 or int(version[2]) > 3: # for the new version like 0.4.0, 0.5.0 and 1.0.0
+                    running_loss += loss.item() * now_batch_size
+                else :  # for the old version like 0.3.0 and 0.3.1
+                    running_loss += loss.data[0] * now_batch_size
+                running_corrects += float(torch.sum(preds == labels.data))
 
             epoch_loss = running_loss / dataset_sizes[phase]
             epoch_acc = running_corrects / dataset_sizes[phase]
             
             print('{} Loss: {:.4f} Acc: {:.4f}'.format(
                 phase, epoch_loss, epoch_acc))
+
             
             y_loss[phase].append(epoch_loss)
             y_err[phase].append(1.0-epoch_acc)            
             # deep copy the model
-            if phase == 'val':
-                last_model_wts = model.state_dict()
-                if epoch%10 == 9:
-                    save_network(model, epoch)
-                draw_curve(epoch)
+            if epoch%10 == 9:
+                if len(opt.gpu_ids)>1:
+                    save_network(model.module, opt.name, epoch+1)
+                else:
+                    save_network(model, opt.name, epoch+1)
+            draw_curve(epoch)
 
+        time_elapsed = time.time() - since
+        print('Training complete in {:.0f}m {:.0f}s'.format(
+            time_elapsed // 60, time_elapsed % 60))
         print()
 
     time_elapsed = time.time() - since
     print('Training complete in {:.0f}m {:.0f}s'.format(
         time_elapsed // 60, time_elapsed % 60))
     #print('Best val Acc: {:4f}'.format(best_acc))
+    save_network(model_test, opt.name+'adapt', epoch)
 
-    # load best model weights
-    model.load_state_dict(last_model_wts)
-    save_network(model, 'last')
     return model
 
 
@@ -238,23 +290,13 @@ ax1 = fig.add_subplot(122, title="top1err")
 def draw_curve(current_epoch):
     x_epoch.append(current_epoch)
     ax0.plot(x_epoch, y_loss['train'], 'bo-', label='train')
-    ax0.plot(x_epoch, y_loss['val'], 'ro-', label='val')
+    #ax0.plot(x_epoch, y_loss['val'], 'ro-', label='val')
     ax1.plot(x_epoch, y_err['train'], 'bo-', label='train')
-    ax1.plot(x_epoch, y_err['val'], 'ro-', label='val')
+    #ax1.plot(x_epoch, y_err['val'], 'ro-', label='val')
     if current_epoch == 0:
         ax0.legend()
         ax1.legend()
     fig.savefig( os.path.join('./model',name,'train.jpg'))
-
-######################################################################
-# Save model
-#---------------------------
-def save_network(network, epoch_label):
-    save_filename = 'net_%s.pth'% epoch_label
-    save_path = os.path.join('./model',name,save_filename)
-    torch.save(network.cpu().state_dict(), save_path)
-    if torch.cuda.is_available:
-        network.cuda(gpu_ids[0])
 
 
 ######################################################################
@@ -263,53 +305,39 @@ def save_network(network, epoch_label):
 #
 # Load a pretrainied model and reset final fully connected layer.
 #
+if not opt.resume:
+    if opt.use_dense:
+        model = ft_net_dense(len(class_names), opt.droprate)
+    elif opt.use_NAS:
+        model = ft_net_NAS(len(class_names), opt.droprate)
+    else:
+        model = ft_net(len(class_names), droprate = opt.droprate, stride = opt.stride, pool = 'avg')
 
-if opt.use_dense:
-    model = ft_net_dense(len(class_names))
-else:
-    model = ft_net(len(class_names))
+    if opt.PCB:
+        model = PCB(len(class_names))
 
-if opt.PCB:
-    model = PCB(len(class_names))
+    opt.nclasses = len(class_names)
 
 print(model)
+# For resume:
+if start_epoch>=40:
+    opt.lr = opt.lr*0.1
 
-if use_gpu:
-    model = model.cuda()
-
-criterion = nn.CrossEntropyLoss()
-
-if not opt.PCB:
-    ignored_params = list(map(id, model.model.fc.parameters() )) + list(map(id, model.classifier.parameters() ))
+if len(opt.gpu_ids)>1:
+    model = torch.nn.DataParallel(model, device_ids=opt.gpu_ids).cuda()
+    ignored_params = list(map(id, model.module.classifier.parameters() ))
     base_params = filter(lambda p: id(p) not in ignored_params, model.parameters())
     optimizer_ft = optim.SGD([
-             {'params': base_params, 'lr': 0.01},
-             {'params': model.model.fc.parameters(), 'lr': 0.1},
-             {'params': model.classifier.parameters(), 'lr': 0.1}
+             {'params': base_params, 'lr': 0.1*opt.lr},
+             {'params': model.module.classifier.parameters(), 'lr': opt.lr}
          ], weight_decay=5e-4, momentum=0.9, nesterov=True)
 else:
-    ignored_params = list(map(id, model.model.fc.parameters() ))
-    ignored_params += (list(map(id, model.classifier0.parameters() )) 
-                     +list(map(id, model.classifier1.parameters() ))
-                     +list(map(id, model.classifier2.parameters() ))
-                     +list(map(id, model.classifier3.parameters() ))
-                     +list(map(id, model.classifier4.parameters() ))
-                     +list(map(id, model.classifier5.parameters() ))
-                     #+list(map(id, model.classifier6.parameters() ))
-                     #+list(map(id, model.classifier7.parameters() ))
-                      )
+    model = model.cuda()
+    ignored_params = list(map(id, model.classifier.parameters() ))
     base_params = filter(lambda p: id(p) not in ignored_params, model.parameters())
     optimizer_ft = optim.SGD([
-             {'params': base_params, 'lr': 0.01},
-             {'params': model.model.fc.parameters(), 'lr': 0.1},
-             {'params': model.classifier0.parameters(), 'lr': 0.1},
-             {'params': model.classifier1.parameters(), 'lr': 0.1},
-             {'params': model.classifier2.parameters(), 'lr': 0.1},
-             {'params': model.classifier3.parameters(), 'lr': 0.1},
-             {'params': model.classifier4.parameters(), 'lr': 0.1},
-             {'params': model.classifier5.parameters(), 'lr': 0.1},
-             #{'params': model.classifier6.parameters(), 'lr': 0.01},
-             #{'params': model.classifier7.parameters(), 'lr': 0.01}
+             {'params': base_params, 'lr': 0.1*opt.lr},
+             {'params': model.classifier.parameters(), 'lr': opt.lr}
          ], weight_decay=5e-4, momentum=0.9, nesterov=True)
 
 # Decay LR by a factor of 0.1 every 40 epochs
@@ -319,17 +347,31 @@ exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=40, gamma=0.1)
 # Train and evaluate
 # ^^^^^^^^^^^^^^^^^^
 #
-# It should take around 15-25 min on CPU. On GPU though, it takes less than a
-# minute.
+# It should take around 1-2 hours on GPU. 
 #
 dir_name = os.path.join('./model',name)
-if not os.path.isdir(dir_name):
-    os.mkdir(dir_name)
-
+if not opt.resume:
+    if not os.path.isdir(dir_name):
+        os.mkdir(dir_name)
+#record every run
+    copyfile('./train.py', dir_name+'/train.py')
+    copyfile('./model.py', dir_name+'/model.py')
 # save opts
-with open('%s/opts.json'%dir_name,'w') as fp:
-    json.dump(vars(opt), fp, indent=1)
+    with open('%s/opts.yaml'%dir_name,'w') as fp:
+        yaml.dump(vars(opt), fp, default_flow_style=False)
 
-model = train_model(model, criterion, optimizer_ft, exp_lr_scheduler,
-                       num_epochs=60)
+# model to gpu
+model = model.cuda()
+if fp16:
+    model, optimizer_ft = amp.initialize(model, optimizer_ft, opt_level = "O1")
 
+criterion = nn.CrossEntropyLoss()
+if opt.moving_avg<1.0:
+    model_test = copy.deepcopy(model)
+    num_epochs = 70
+else:
+    model_test = None
+    num_epochs = 60
+
+model = train_model(model, model_test, criterion, optimizer_ft, exp_lr_scheduler,
+                       num_epochs=num_epochs)
